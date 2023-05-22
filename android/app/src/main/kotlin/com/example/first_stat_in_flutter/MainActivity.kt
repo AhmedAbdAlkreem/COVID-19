e5c4e25619abbc35716d72598d855ad47af7f1d0
Ahmed Abd Alkreem <109450704+AhmedAbdAlkreem@users.noreply.github.com>
2023-05-22 13:53:04 +0300

    COVID-19

---
+++ android/app/src/main/kotlin/com/example/first_stat_in_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_stat_in_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+import 'MlModel.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget
+{
+  @override
+  State<SplashScreen> createState()=>_SplashScreen();
+
+
+}
+class _SplashScreen extends State<SplashScreen> {
+  var _time;
+  start()
+  {
+    _time=Timer(Duration(seconds: 6), call);
+  }
+  void call()
+  {
+    Navigator.pushReplacement(context,
+      MaterialPageRoute(builder: (context)=>MlModel(),),
+    );
+  }
+  @override
+  void initState()
+  {
+    start();
+    super.initState();
+  }
+  @override
+  void dispose()
+  {
+    _time.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff55598d),
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarBrightness: Brightness.dark,
+            statusBarColor: Colors.black,
+            systemNavigationBarColor: Colors.black
+        ),
+        backgroundColor: Color(0xff55598d),
+        elevation: 0,
+
+      ),
+      body: Center(child:Lottie.network('https://assets5.lottiefiles.com/packages/lf20_itilDAyVNt.json')),
+    );
+  }
+}
+
+//https://assets5.lottiefiles.com/packages/lf20_itilDAyVNt.json
